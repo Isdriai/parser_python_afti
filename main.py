c8c819bdb05bcf015448b222d4b41ac7a2170dbf
PicotRodolphe <picot.rodolphe@gmail.com>
2017-10-28 20:15:56 +0200

    correction

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
-
+from mbr import mbr
+from fat32Directory import fat32Directory
+from fat32InfoSector import fat32InfoSector
+from fat32SectorBoot import fat32SectorBoot
+from goto import goto, label
 
 def main():
 	sections={}
 	fichier = input("Rentrez le nom du fichier à parser\n")
 	taille = os.path.getsize(fichier)
 	for adresse in range(0, taille, 0x10):
-		
+		# Attention, tester le fat32SectorBoot avant le mbr car ils ont le meme number magic, mais le fat32SectorBoot
+		# a besoin d'un deuxieme element pour etre identifié
+		for forme in [fat32SectorBoot, mbr, fat32Directory, fat32Directory, fat32Directory]:
+			try:
+				test_parse=forme(fichier, adresse)
+				sections.add(test_parse)
+			except NameError:
+				pass
 
 if __name__ == '__main__':
 	main()
